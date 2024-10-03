13e3dc81129473e4a21b81f399a62d4d6d1717c5
lenlino <65199014+lenlino@users.noreply.github.com>
2024-10-04 08:34:21 +0900

    support v2

--- Tweetron_Core.py
+++ Tweetron_Core.py
@@ -61,50 +61,52 @@ from tweetron_library import webscript_save, utility, tweetron_variable
 import api_key
 
 import software_info
+
 version = software_info.VERSION()
 
+parser = argparse.ArgumentParser(description='Tweetron ver' + version)
 
-
-parser = argparse.ArgumentParser(description = 'Tweetron ver' + version)
-
-parser.add_argument('--preset_name', required = True)
-parser.add_argument('--debug_mode', required = True)
+parser.add_argument('--preset_name', required=True)
+parser.add_argument('--debug_mode', required=True)
 args = parser.parse_args()
 preset_name = args.preset_name
 debugmode_sw = bool(int(args.debug_mode))
 
-
 #original_src: demoji(https://github.com/bsolomon1124/demoji)
-demoji_json_open = open('tweetron_data/emoji_codes.json', 'r', encoding = 'utf-8')
+demoji_json_open = open('tweetron_data/emoji_codes.json', 'r', encoding='utf-8')
 demoji_json_load = json.load(demoji_json_open)
 
-def delete_emoji(original_string):
 
+def delete_emoji(original_string):
     global demoji_json_load
 
-    escp = (re.escape(c) for c in sorted(demoji_json_load, key = len, reverse = True))
+    escp = (re.escape(c) for c in sorted(demoji_json_load, key=len, reverse=True))
     emoji_pattern = re.compile(r"|".join(escp))
 
     return emoji_pattern.sub('', original_string)
 
+
 colorama.init()
+
 
 def print_info(text):
     print(colored('[INFO] ', 'green'), text)
 
+
 def print_warning(text):
     print(colored('[WARNING] ', 'yellow'), text)
+
 
 def print_error(text):
     print(colored('[ERROR] ', 'red'), text)
 
+
 settingvalue_dict_all = tweetron_variable.reset_settingvalue_dict_all(datetime)
 datetime_now = datetime.datetime.now()
 
-
-
 consumer_key = api_key.CONSUMER_KEY()
 consumer_secret = api_key.CONSUMER_SECRET()
+bearer_token = api_key.BEARER_TOKEN()
 callback_url = 'oob'
 
 main_config = configparser.ConfigParser()
@@ -115,11 +117,9 @@ access_token_secret = main_config.get('TwitterAPI', 'access_token_secret')
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret, callback=callback_url)
 
 auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth, wait_on_rate_limit=True)
+api = tweepy.Client(bearer_token, consumer_key, consumer_secret, access_token, access_token_secret)
 
 port_number = main_config.get('MainConfig', 'portnumber')
-
-
 
 tweet_send_cnt = 0
 searchword_loop_cnt = 0
@@ -135,8 +135,6 @@ loadloop_sw = 0
 nogood_word_list = ''
 current_client_id = 0
 
-
-
 os.system('title Tweetron v' + version)
 
 print(colored(figlet_text, 'cyan'))
@@ -144,10 +142,8 @@ print(colored('Version: ' + version + '\n', 'green'))
 print(colored('Developed by Tweetron Developers', 'cyan'))
 print(colored('https://github.com/Tweetron/\n', 'cyan'))
 
-
-
 read_main_config = configparser.RawConfigParser()
-read_main_config.read('tweetron_data/preset/' + preset_name  + '/config.ini')
+read_main_config.read('tweetron_data/preset/' + preset_name + '/config.ini')
 
 for key_p_name in settingvalue_dict_all.keys():
     for key_c_name in settingvalue_dict_all[key_p_name].keys():
@@ -156,16 +152,12 @@ for key_p_name in settingvalue_dict_all.keys():
         else:
             settingvalue_dict_all[key_p_name][key_c_name] = read_main_config.get(key_p_name, key_c_name)
 
-
-
 with open('tweetron_data/preset/' + preset_name + '/search_word.txt') as file:
     for i in range(sum(1 for line in open('tweetron_data/preset/' + preset_name + '/search_word.txt'))):
         if i == 0:
             search_word = file.readline().rstrip(os.linesep)
         else:
             search_word = search_word + ' OR ' + file.readline().rstrip(os.linesep)
-
-
 
 with open('tweetron_data/preset/' + preset_name + '/nogood_word.txt') as file:
     nogood_word = file.readlines()
@@ -174,25 +166,38 @@ for ngword in nogood_word:
     nogood_word_list = nogood_word_list + ngword + ', '
 
 loadloop_sw = settingvalue_dict_all['display_setting']['text_loadloop']
-reply_exclusion_text = 'exclude:replies' if settingvalue_dict_all['main_setting']['reply_exclusion'] == 1 else ''
-search_command = '' if settingvalue_dict_all['filter_setting']['search_command'] == 'null' else settingvalue_dict_all['filter_setting']['search_command']
-since_date = datetime_now.strftime('%Y-%m-%d_%H:%M:%S_JST') if settingvalue_dict_all['main_setting']['since_rb'] == 1 else settingvalue_dict_all['main_setting']['searchdate'] + '_' + str(settingvalue_dict_all['main_setting']['searchdate_h']).zfill(2) + ':' + str(settingvalue_dict_all['main_setting']['searchdate_m']).zfill(2) + ':' + str(settingvalue_dict_all['main_setting']['searchdate_s']).zfill(2) + '_JST'
+reply_exclusion_text = '-is:reply' if settingvalue_dict_all['main_setting']['reply_exclusion'] == 1 else ''
+search_command = '' if settingvalue_dict_all['filter_setting']['search_command'] == 'null' else \
+    settingvalue_dict_all['filter_setting']['search_command']
 
-search_text_raw = search_word + ' -filter:retweets since:' + since_date + ' ' + reply_exclusion_text + ' ' + search_command
-
-
+search_text_raw = search_word + ' -is:retweet ' + reply_exclusion_text + ' ' + search_command
 
 print_info('再起動をした場合は再度[ Tweetron.html ]を読み込んでください')
 if debugmode_sw == True: print_info('デバッグモード: 有効')
 print_info('使用プリセット: ' + preset_name + ' ポート番号: ' + str(port_number))
 print_info('検索ワード: ' + search_text_raw)
 
-print_info('NGワードは設定されていません') if nogood_word_list == '' else print_info('NGワードリスト: ' + nogood_word_list)
+print_info('NGワードは設定されていません') if nogood_word_list == '' else print_info(
+    'NGワードリスト: ' + nogood_word_list)
 
+
+def get_data_time():
+    return (datetime_now - datetime.timedelta(seconds=15)).strftime('%Y-%m-%dT%H:%M:%S+09:00') if \
+    settingvalue_dict_all['main_setting'][
+        'since_rb'] == 1 else \
+        settingvalue_dict_all['main_setting']['searchdate'] + 'T' + str(
+            settingvalue_dict_all['main_setting']['searchdate_h']).zfill(2) + ':' + str(
+            settingvalue_dict_all['main_setting']['searchdate_m']).zfill(2) + ':' + str(
+            settingvalue_dict_all['main_setting']['searchdate_s']).zfill(2) + '+09:00'
+
+
+def search_user(users, id):
+    for user in users:
+        if id == user.id:
+            return user
 
 
 def search_word_api(si):
-
     tweet_list = []
     tweet_text_raw = ''
 
@@ -205,16 +210,21 @@ def search_word_api(si):
     global since_rb
 
     searchword_loop_cnt = 0
+    since_date = get_data_time()
+    print(search_text_raw)
 
     #ワード検索
     if si == '0':
-        tweets = api.search_tweets(q = search_text_raw, result_type = 'recent', count = random.randint(10,30), include_entities = False, tweet_mode = 'extended')
+        tweets = api.search_recent_tweets(query=search_text_raw, max_results=10, start_time=since_date, tweet_fields=["author_id"],
+                                          expansions=["author_id"], user_fields=["id", "username"])
     else:
         #前回取得したツイートより最新のツイートを取得する
-        tweets = api.search_tweets(q = search_text_raw, result_type = 'recent', count = random.randint(5,10), include_entities = False, tweet_mode = 'extended', since_id = si)
+        tweets = api.search_recent_tweets(query=search_text_raw, max_results=10, since_id=si, tweet_fields=["author_id"],
+                                          expansions=["author_id"], user_fields=["id", "username"])
+    print(tweets)
 
-    for result in tweets:
-        tweet_text_raw = unescape(result.full_text)
+    for result in tweets.data:
+        tweet_text_raw = unescape(result.text)
 
         ng_flag = 0
 
@@ -222,18 +232,21 @@ def search_word_api(si):
             if nogood_word != '' and nogood_word_if in tweet_text_raw: ng_flag = 1
 
         if ng_flag == 0:
-            if searchword_loop_cnt == 0: relast_id = result.id_str
+            if searchword_loop_cnt == 0: relast_id = result.id
 
             #画像URLを削除
             tweet_text = re.sub(r'https://t.co/\w{10}', '', tweet_text_raw)
 
             if settingvalue_dict_all['main_setting']['emoji_exclusion'] == 1: tweet_text = delete_emoji(tweet_text)
 
+            user = search_user(tweets.includes["users"], result.author_id)
             #それぞれの表示形態に合わせて整形
             if settingvalue_dict_all['display_setting']['text_displaytype'] == 1:
-                tweet_list.append(tweet_text.replace('\n','') +' (@'+ unescape(result.user.name) +')')
+                tweet_list.append(tweet_text.replace('\n', '') + ' (@' + unescape(user.username) + ')')
             else:
-                tweet_list.append(unescape(result.user.name) + ' (@' + unescape(result.user.screen_name) + ')\n' + tweet_text.replace('\n',''))
+                tweet_list.append(
+                    unescape(user.name) + ' (@' + unescape(user.username) + ')\n' + tweet_text.replace(
+                        '\n', ''))
 
             searchword_loop_cnt += 1
 
@@ -242,27 +255,24 @@ def search_word_api(si):
     return tweet_list
 
 
-
 #ツイートID取得
 def checkid_relast():
-
     relast_id_return = ''
     lpcnt = 0
+    since_date = get_data_time()
 
-    tweets = api.search_tweets(q = search_text_raw, result_type = 'recent', count = 3, include_entities = False)
+    tweets = api.search_recent_tweets(query=search_text_raw, max_results=10, start_time=since_date)
 
-    for result in tweets:
-        if lpcnt == 0: relast_id_return = result.id_str
+    for result in tweets.data:
+        if lpcnt == 0: relast_id_return = result.id
 
         lpcnt += 1
 
     return relast_id_return
 
 
-
 #JSと接続
 def new_client(client, server):
-
     dt_now = datetime.datetime.now()
 
     global tweet_send_cnt
@@ -298,21 +308,19 @@ def new_client(client, server):
         print_warning('ツイートが存在しません テストツイートを行い、しばらくしてからページを再読込してください')
 
 
-
 #JSから切断
 def client_left(client, server):
     dt_now = datetime.datetime.now()
 
     if current_client_id != client['id']:
-        print_warning(dt_now.strftime('%H:%M:%S') + ' server.ID[' + str(client['id']) + '] - クライアントの重複により切断されました')
+        print_warning(dt_now.strftime('%H:%M:%S') + ' server.ID[' + str(
+            client['id']) + '] - クライアントの重複により切断されました')
     else:
         print_info(dt_now.strftime('%H:%M:%S') + ' server.ID[' + str(client['id']) + '] - 切断されました')
 
 
-
 #JSからのメッセージ受信
 def message_received(client, server, message):
-
     global tweet_send_cnt
     global random_list
     global tweet_list_text
@@ -328,7 +336,8 @@ def message_received(client, server, message):
     #JSから送信が来た場合ツイートを送る
     if message == 'JS':
 
-        if debugmode_sw == True: print_info('現在のツイート数: ' + str(len(tweet_list_text)) + ' / 現在の最新id:' + str(relast_id) + ' / 控えid: ' + str(relast_id_tmp))
+        if debugmode_sw == True: print_info('現在のツイート数: ' + str(len(tweet_list_text)) + ' / 現在の最新id:' + str(
+            relast_id) + ' / 控えid: ' + str(relast_id_tmp))
 
         #searchword_loop_cnt = 0 は取得できるツイートが存在しない
         if searchword_loop_cnt != 0:
@@ -357,7 +366,6 @@ def message_received(client, server, message):
                         while True:
 
                             if bool(tweet_list_text):
-
                                 tweet_text_tmp = random.choice(tweet_list_text)
                                 tweet_list_text.remove(tweet_text_tmp)
 
@@ -383,7 +391,6 @@ def message_received(client, server, message):
                             tweet_list_text = search_word_api('0')
 
                             if bool(tweet_list_text):
-
                                 tweet_text_tmp = random.choice(tweet_list_text)
                                 tweet_list_text.remove(tweet_text_tmp)
 
@@ -417,7 +424,6 @@ def message_received(client, server, message):
                                 tweet_list_text = search_word_api(relast_id)
 
                                 if bool(tweet_list_text):
-
                                     tweet_text_tmp = random.choice(tweet_list_text)
                                     tweet_list_text.remove(tweet_text_tmp)
 
@@ -459,8 +465,7 @@ def message_received(client, server, message):
             print_warning('ツイートが存在しません テストツイートを行い、しばらくしてからページを再読込してください')
 
 
-
-server = WebsocketServer(port = int(port_number))
+server = WebsocketServer(port=int(port_number))
 
 server.set_fn_new_client(new_client)
 server.set_fn_client_left(client_left)
